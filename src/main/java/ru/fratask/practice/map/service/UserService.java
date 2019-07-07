c6f078961050fe51a302f7287ee7d79d58f4bcde
Fratask <amir.akhatov99@gmail.com>
2019-07-07 21:15:22 +0300

    Created UserService interface

--- src/main/java/ru/fratask/practice/map/service/UserService.java
+++ src/main/java/ru/fratask/practice/map/service/UserService.java
@@ -1,4 +1,18 @@
 package ru.fratask.practice.map.service;
 
+import ru.fratask.practice.map.entity.User;
+
+import java.util.List;
+
 public interface UserService {
+
+    User register(User user);
+
+    User findByUsername(String username);
+
+    User findById(Long id);
+
+    List<User> getAll();
+
+    void delete(Long id);
 }
