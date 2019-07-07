ed87099a28a173d6f0c0c23c9fea131e6711ac67
Fratask <amir.akhatov99@gmail.com>
2019-07-07 21:11:02 +0300

    Created entity.User.class

--- src/main/java/ru/fratask/practice/map/entity/User.java
+++ src/main/java/ru/fratask/practice/map/entity/User.java
@@ -1,4 +1,20 @@
 package ru.fratask.practice.map.entity;
 
-public interface User {
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
 }
